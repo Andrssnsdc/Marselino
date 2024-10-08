05c00d8b52edf00258b14b3fe391b1c1f20d4333
Andersson <anderssonstiven04@gmail.com>
2024-10-08 17:35:54 -0500

    se corrigio error de entrada de datos dinamicos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { server } from 'typescript';
 
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
    
 });
